82b58811dbf838993c777ffeeff933348b062bf1
Luis Felipe Bertel Mercado <ellipe@gmail.com>
2020-01-14 21:18:12 -0500

    added validations and formating some responses using fmt

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello World from Snippetbox"))
 }
 
 // Add a showSnippet handler function.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	// Attemtps to convert the URL Query String into an Integer
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	// Validates if id is bigger than 0
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID... %d", id)
 }
 
 // Add a createSnippet handler function.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		// If it's not, then send a 405 status response code indicating that it is
+		// not allowed.
+		w.Header().Set("Allow", http.MethodPost)
+		// w.WriteHeader(http.StatusMethodNotAllowed)
+		// w.Write([]byte("Method Not allowed"))
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
